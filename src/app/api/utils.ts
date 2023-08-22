29c34f112c11e3d305bb8584ee9f8ba9cd0becaf
CarlosArrieta <carrieta@defontana.com>
2023-08-22 13:30:26 -0300

    add origin

--- src/app/api/utils.ts
+++ src/app/api/utils.ts
@@ -4,7 +4,7 @@ export const OkResponse = (data: any) => {
   return new NextResponse(data, {
     status: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": "http://localhost:3000/",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
     },
