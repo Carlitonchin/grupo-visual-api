5122ee96aa16067b4d5fb706ba030d0956303964
CarlosArrieta <carrieta@defontana.com>
2023-09-20 12:33:30 -0300

    add installments

--- src/app/api/buy-courses/route.ts
+++ src/app/api/buy-courses/route.ts
@@ -1,6 +1,5 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import { NextApiRequest } from "next";
 import { OkResponse } from "../utils";
 import axios from "axios";
 import { courses } from "../data";
@@ -15,17 +14,18 @@ const headers = {
   authorization: "Basic " + Buffer.from(`${key}:`).toString("base64"),
 };
 
-let payments = [
+let payments: any[] = [
   {
     payment_method: "checkout",
     checkout: {
       customer_editable: true,
       skip_checkout_success_page: false,
       accepted_payment_methods: ["credit_card", "pix"],
-      success_url: url_site + "obrigado",
+      success_url: url_site,
       credit_card: {
         statement_descriptor: "Grupo Visual",
         operation_type: "auth_and_capture",
+        installments: [],
       },
       pix: {
         expires_in: 100000,
@@ -43,8 +43,9 @@ function createItems(body: any) {
     if (!c) {
       throw new Error("curso no encontrado");
     }
+
     items.push({
-      amount: c.price * 100,
+      amount: Math.round(c.price * 100),
       description: c.text,
       quantity: cant,
       code: c.id + "",
@@ -54,11 +55,43 @@ function createItems(body: any) {
   return items;
 }
 
+function createInstallMents(items: any[]): any[] {
+  let installments = [];
+  for (let i = 1; i <= 12; i++) {
+    let inst = {
+      number: i + "",
+      total: 0,
+    };
+
+    for (let j = 0; j < items.length; j++) {
+      let c = courses.find((x) => x.id == Number(items[j].code));
+      if (!c) {
+        throw new Error("curso no encontrado");
+      }
+
+      let price = Number(items[j].amount);
+      const freeTax = c.freeTax || 1;
+      if (i > freeTax) {
+        const taxes = Math.round(price * (c.taxes / 100)) * (i - freeTax);
+        price += taxes;
+      }
+
+      inst.total += price;
+    }
+
+    installments.push(inst);
+  }
+
+  return installments;
+}
+
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
-
     let items = createItems(body);
+    const installments = createInstallMents(items);
+    payments[0].checkout.credit_card.installments = installments;
+
     const resp = await axios.post(
       url_pagarme,
       { items, payments, customer: body.customer },
@@ -76,7 +109,7 @@ export async function GET(req: NextRequest) {
     const url = new URL(req.url);
     const searchParams = new URLSearchParams(url.search);
     const getUrl = url_pagarme + "/" + searchParams.get("order");
-    console.log(getUrl);
+
     const resp = await axios.get(getUrl, { headers });
     return OkResponse(resp.data);
   } catch (error) {
